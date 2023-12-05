49c0ba54187e1b53b708cd0284d140de996ca62f
marmarkaroo <masayaf8@gmail.com>
2023-12-05 15:51:19 -0800

    added lessons title and began formatting

--- src/views/Lessons.vue
+++ src/views/Lessons.vue
@@ -2,7 +2,30 @@
 </script>
 
 <template>
-  <main>
- 
-  </main>
+  <div class="title">
+    <h1>Wolf Hack Lessons</h1>
+  </div>
+  <div class="lessons">
+    <h2>This page contains all the lessons of Wolf Hack Academy, including Basics, Hashcat, and Wireshark. Each lesson will contain a brief description of the topic, some examples, and end with a graded quiz to test your understanding.</h2>
+  </div>
 </template>
+
+<style>
+  .title {
+    max-width: 300px;
+    padding-top: 200px;
+    margin: 0 auto;
+    padding: 20px;
+    background-color: silver;
+    border-radius: 10px;
+    text-align: center;
+    color: rgb(0, 0, 72);
+    margin-bottom: 20px;
+  }
+
+  .lessons {
+    text-align: center;
+    color: rgb(0, 0, 72);
+    margin-bottom: 20px;
+  }
+</style>
